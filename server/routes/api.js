255e113ded792e9b37105b8ec7619b0c5d8b7c20
samliweisen <liweisen007@gmail.com>
2018-09-02 20:20:05 +0000

    set read request

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,8 +5,21 @@ const Build = require('../models/build');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-    Build.find({}).exec((builds) => {
+    Build.find({}).exec((err, builds) => {
         res.send(builds);
+    });
+});
+
+router.get('/read', (req, res) => {
+    const bundle_id = req.query.bundle_id;
+    Build.findOne({bundle_id: bundle_id}, (err, build) => {
+        if (!err && build) {
+            console.log('exist');
+            res.send({build_number: build.build_number});
+        } else {
+            console.log('not exist');
+            res.sendStatus(400);
+        }
     });
 });
 
@@ -14,14 +27,18 @@ router.post('/set', (req, res) => {
     const bundle_id = req.body.bundle_id;
     let new_build_number = req.body.new_build_number;
     let newBuild;
-    Build.find({bundle_id: bundle_id}, (build) => {
+    Build.findOne({bundle_id: bundle_id}, (err, build) => {
         if (build) {
-            build.build_number += 1;
-            res.send(build);
+            if (new_build_number > build.build_number) {
+                build.build_number = new_build_number;
+            }
+            Build.findOneAndUpdate({_id: build._id}, build, {upsert: true}, function(err, build) {
+                res.send(build);
+            });
         } else {
             newBuild = new Build({
                 bundle_id: bundle_id,
-                build_number: new_build_number
+                build_number: 0
             });
             newBuild.save(function(err, build) {
                 if (err) {
@@ -32,6 +49,10 @@ router.post('/set', (req, res) => {
             });
         }
     });
+});
+
+router.post('/bump', (req, res) => {
+    
 });
 
 module.exports = router;
